1dd14b30518fe3ed564bb91a334c68635fdbe30d
David Morton <davidlmorton@gmail.com>
2014-10-18 16:10:18 -0500

    Make layout more consistent with petri service

--- ptero_shell_command/api/wsgi.py
+++ ptero_shell_command/api/wsgi.py
@@ -1,3 +1,17 @@
-from . import application
+from ptero_shell_command.api import application
+import argparse
 
 app = application.create_app()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--host', type=str, default='localhost')
+    parser.add_argument('--debug', action='store_true', default=False)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    app.run(host=args.host, port=args.port, debug=args.debug)
