f1b35281c258b3a1043c8d06862b6f597d8aaba5
David Morton <davidlmorton@gmail.com>
2014-10-18 16:12:49 -0500

    Add logging configuration from ptero_common

--- ptero_shell_command/api/wsgi.py
+++ ptero_shell_command/api/wsgi.py
@@ -1,4 +1,5 @@
 from ptero_shell_command.api import application
+from ptero_common.logging_configuration import configure_logging
 import argparse
 
 app = application.create_app()
@@ -14,4 +15,6 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
+    configure_logging(level_env_var='PTERO_SHELL_COMMAND_LOG_LEVEL',
+            time_env_var='PTERO_SHELL_COMMAND_LOG_TIME')
     app.run(host=args.host, port=args.port, debug=args.debug)
