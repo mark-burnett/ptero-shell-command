31c316e7ce92e4b865294494e372b77820051b6d
Mark Burnett <mark.m.burnett@gmail.com>
2014-11-21 13:45:57 -0600

    test that required params are present

--- tests/api/v1/test_bad_requests.py
+++ tests/api/v1/test_bad_requests.py
@@ -3,6 +3,23 @@ import simplejson
 
 
 class TestBadRequests(BaseAPITest):
-    def test_empty_body(self):
-        post_response = self.post(self.jobs_url, {})
+    def setUp(self):
+        super(TestBadRequests, self).setUp()
+
+        self.valid_request_data = {
+            'commandLine': ['true'],
+            'user': self.job_user,
+            'workingDirectory': self.job_working_directory,
+        }
+
+    def _expect_400(self, data):
+        post_response = self.post(self.jobs_url, data)
         self.assertEqual(400, post_response.status_code)
+
+    def test_empty_body(self):
+        self._expect_400({})
+
+    def test_missing_required_parameters(self):
+        for param in ['commandLine', 'user', 'workingDirectory']:
+            self.valid_request_data.pop(param)
+            self._expect_400(self.valid_request_data)
