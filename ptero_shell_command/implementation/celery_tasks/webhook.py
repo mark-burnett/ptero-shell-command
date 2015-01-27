a2dc3382e3a972d28fc2565d9afa48827889b048
Michael J. Kiwala <michael.kiwala@gmail.com>
2015-01-27 11:54:30 -0600

    use common logging configuration for celery

--- ptero_shell_command/implementation/celery_tasks/webhook.py
+++ ptero_shell_command/implementation/celery_tasks/webhook.py
@@ -1,15 +1,19 @@
+from ptero_common.logging_configuration import logged_request
 import celery
+import logging
 import requests
 import json
 
+LOG = logging.getLogger(__name__)
 __all__ = ['WebhookTask']
 
 
 class WebhookTask(celery.Task):
     ignore_result = True
     def run(self, url, **kwargs):
-        response = requests.put(url, data=self.body(kwargs),
-                headers={'Content-Type': 'application/json'})
+        response = logged_request.put(
+            url, data=self.body(kwargs),
+            headers={'Content-Type': 'application/json'}, logger=LOG)
 
     def body(self, kwargs):
         return json.dumps(kwargs)
