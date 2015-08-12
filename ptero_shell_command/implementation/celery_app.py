e7e9f35254860e605917633803ef0e49f02586fc
Michael J. Kiwala <michael.kiwala@gmail.com>
2015-08-12 11:31:34 -0500

    Move celery app creation into ptero_common

--- ptero_shell_command/implementation/celery_app.py
+++ ptero_shell_command/implementation/celery_app.py
@@ -1,38 +1,19 @@
 from celery.signals import setup_logging
 from ptero_common.logging_configuration import configure_celery_logging
-import celery
+from ptero_common.celery.app import celery_app
 import os
 
 
 TASK_PATH = 'ptero_shell_command.implementation.celery_tasks'
-
-
-app = celery.Celery('PTero-shell-command-celery', include=TASK_PATH)
-
-
-app.conf['CELERY_ROUTES'] = (
+routes = (
     {
         TASK_PATH + '.shell_command.ShellCommandTask': {'queue': 'fork'},
         'ptero_common.celery.http.HTTP': {'queue': 'http'},
     },
 )
-
-
-_DEFAULT_CELERY_CONFIG = {
-    'CELERY_BROKER_URL': 'amqp://localhost',
-    'CELERY_RESULT_BACKEND': 'redis://localhost',
-    'CELERY_ACCEPT_CONTENT': ['json'],
-    'CELERY_ACKS_LATE': True,
-    'CELERY_RESULT_SERIALIZER': 'json',
-    'CELERY_TASK_SERIALIZER': 'json',
-    'CELERY_TRACK_STARTED': True,
-    'CELERYD_PREFETCH_MULTIPLIER': 10,
-}
-for var, default in _DEFAULT_CELERY_CONFIG.iteritems():
-    if var in os.environ:
-        app.conf[var] = os.environ[var]
-    else:
-        app.conf[var] = default
+broker_url = os.environ.get('PTERO_SHELL_COMMAND_CELERY_BROKER_URL')
+app = celery_app(main='PTero-shell-command-celery', task_path=TASK_PATH,
+    routes=routes, broker_url=broker_url, track_started=True)
 
 
 @setup_logging.connect
