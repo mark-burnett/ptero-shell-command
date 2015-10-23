fbb2989456756d93d8936e230524f428dfad9b9c
David Morton <davidlmorton@gmail.com>
2015-10-23 04:27:12 +0000

    pretty format for json response bodies

--- ptero_shell_command/api/v1/api.py
+++ ptero_shell_command/api/v1/api.py
@@ -1,9 +1,12 @@
 from flask.ext.restful import Api
+from flask.ext.restful.representations import json as flask_json
 from . import views
 
 __all__ = ['api']
 
-
 api = Api(default_mediatype='application/json')
+flask_json.settings['indent'] = 4
+flask_json.settings['sort_keys'] = True
+
 api.add_resource(views.JobListView, '/jobs', endpoint='job-list')
 api.add_resource(views.JobView, '/jobs/<string:pk>', endpoint='job')
