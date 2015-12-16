3ed05914b67826cc700be9896fba4233de2bf1b1
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-12-16 20:25:32 +0000

    Use hasattr to detect whether the backend has been setup

--- ptero_shell_command/api/application.py
+++ ptero_shell_command/api/application.py
@@ -43,4 +43,5 @@ def _attach_factory_to_app(factory, app):
 
     @app.teardown_request
     def teardown_request(exception):
-        flask.g.backend.cleanup()
+        if hasattr(flask.g, 'backend'):
+            flask.g.backend.cleanup()
