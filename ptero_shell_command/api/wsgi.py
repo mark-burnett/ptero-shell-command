3fbee27e5b948e4511dc6febe8831801fa491ee1
David Morton <davidlmorton@gmail.com>
2016-03-02 20:17:52 +0000

    Handle SIGTERM in wsgi.py so coverage is measured

--- ptero_shell_command/api/wsgi.py
+++ ptero_shell_command/api/wsgi.py
@@ -5,7 +5,21 @@ import os
 app = application.create_app()
 configure_web_logging('SHELL_COMMAND')
 
+
+def handle_sigterm(signum, frame):
+    import sys
+    sys.stderr.write('Handling SIGTERM... shutting down the Flask Server')
+    shutdown_server()
+
+
+def shutdown_server():
+    raise RuntimeError('Forcefully shutting down the Flask Server')
+
+
 if __name__ == '__main__':
+    import signal
+    signal.signal(signal.SIGTERM, handle_sigterm)
+
     app.run(host='0.0.0.0',
             port=os.environ['PTERO_SHELL_COMMAND_PORT'],
             debug=False, use_reloader=False)
